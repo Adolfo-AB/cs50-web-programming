4c7bde10266f16fa4cf28371b2dd14eda77fff69
Adolfo-AB <adolfoab6@gmail.com>
2021-10-20 20:44:46 +0200

    Work in network project

--- L8 Testing and CICD/project4/network/static/network/index.js
+++ L8 Testing and CICD/project4/network/static/network/index.js
@@ -30,6 +30,55 @@ function loadPosts(page, pageNum) {
 function buildPaginator(page, pageNum, totalPageNum) {
     pageList = document.getElementById('pagination');
     pageList.innerHTML = "";
+
+    const prev = document.createElement('li');
+    if (pageNum == 1) {
+        prev.className = "pageitem disabled";
+    } else {
+        prev.className = "page-item";
+        prev.addEventListener('click', () =D loadPosts(page, pageNum-1));
+    }
+
+    const aPrevPage = document.createElement('a');
+    aPrevPage.className = "page-link";
+    aPrevPage.href = "#";
+    aPrevPage.innerHTML = "Older";
+    prev.append(aPrevPage);
+    pageList.append(prev);
+
+    for (let i=1; i<=totalPageNum; i++) {
+        const pageIcon = document.createElement('li');
+
+        if (i == pageNum) {
+            pageIcon.className = "page-item active";
+        } else {
+            pageIcon.className = "page-item";
+            pageIcon.addEventListener('click', () => loadPosts(page,i));
+        }
+
+        const aPage = document.createElement('a');
+        aPage.className = "page-link";
+        aPage.href = "#";
+        aPage.innerHTML = i;
+        pageIcon.append(aPage);
+        pageList.append(pageIcon);
+    }
+
+    const next = document.createElement('li');
+    if (pageNum == totalPageNum) {
+        next.className = "page-item disabled";
+    } else {
+        next.className = "page-item";
+        next.addEventListener('click', () => loadPosts(page, pageNum+1));
+    }
+
+    aNextPage = document.createElement('a');
+    aNextPage.className = "page-link";
+    aNextPage.href = "#";
+    aNextPage.innerHTML = "Newer";
+    next.append(aNextPage);
+    pageList.append(next);
+
 }
 
 function buildPost(post) {
@@ -62,7 +111,113 @@ function buildPost(post) {
     cardDateTime.className = "card-datetime";
     cardDateTime.innerHTML = post.datetime;
     cardBody.append(cardDateTime);
+
+    const likesRow = document.createElement('div');
+    likesRow.id = `likes-row-${post.id}`;
+    likesRow.className = "row align-items-center";
+
+    const likeIcon = document.createElement('i');
+    likeIcon.id = `like-icon-${post.id}`;
+    
+    let heart_bg;
+    if (post.liked) {
+        heart_bg = "";
+    } else {
+        heart_bg = "-empty";
+    }
+
+    likeIcon.className = `icon-heart-${heart_bg} col-auto`;
+    if (document.getElementById('following')) {
+        likeIcon.addEventListener('click', () => updateLike(post));
+    }
+
+    likesRow.append(likeIcon);
+
+    const likes = document.createElement('div');
+    likes.id = `number-likes-${post.id}`;
+    likes.className = "card-text likes col-auto";
+    likes.innerHTML = post.likes;
+    likesRow.append(likes);
+
+    const likeText = document.createElement('div');
+    likeText.className = "card-text likes_text col-auto";
+    likeText.innerHTML = "Likes";
+    likesRow.append(likeText);
+
+    if (post.editable) {
+        const edit = document.createElement('button');
+        edit.className = "card-text col-auto btn btn-link";
+        edit.innerHTML = "Edit";
+        edit.addEventListener('click', () => editPost(post));
+        likesRow.append(edit);
+    }
+
+    cardBody.append(likesRow);
+    postCard.append(cardBody);
+
+    const row = document.createElement('div');
+    row.className = "row justify-content-center";
+    row.append(postCard);
+
+    document.querySelector('#posts').append(row);
 }
+
+function editPost(post) {
+    const likesRow = document.getElementById(`likes-row-${post.id}`);
+    const content = document.getElementById(`content-${post.id}`);
+
+    const postBody = content.parentNode;
+    const editBtnRow = document.createElement('div');
+    editBtnRow.className = "row";
+
+    const saveBtn = document.createElement('button');
+    saveBtn.className = "btn btn-sm btn-outline-primary";
+    saveBtn.type = "button";
+    saveBtn.innerHTML = "Save Changes";
+    saveBtn.addEventListener('click', () => {
+        const newContent = document.getElementById(`new-content-${post.id}`).value;
+        fetch(`/save_post`, {
+            method: 'PUT',
+            headers: {
+                'X-CSRFToken': getCookie("csrftoken")
+            },
+            body: bod = JSON.stringify({
+                post_id: post.id,
+                newContent: new_content
+            })
+        })
+        .then(response => response.json())
+        .then(response => {
+            
+        })
+    })
+}
+
+function updateLike(post) {
+    fetch(`/post/${post.id}/update_like`)
+    .then(response => response.json())
+    .then(response => {
+        if (response.liked) {
+            document.getElementById(`like-icon-${post.id}`).className = "icon-heart col-auto";
+        } else {
+            document.getElementById(`like-icon-${post.id}`).className = "icon-heart-empty col-auto";
+        }
+        document.getElementById(`number-likes-${post.id}`).innerHTML = response.newAmount;
+    })
+}
+
+function updateFollowStatus(userProfileId) {
+    fetch(`/profile/${userProfileId}/update_follow`)
+    .then(response => response.json())
+    .then(response => {
+        followBtn = document.getElementById('follow-button');
+        if (response.newFollower) {
+            newStatus = False
+        }
+    })
+}
+
+
 
 function displayProfile(authorId) {
     loadPosts(`?profile=${authorId}`, 1);
@@ -86,20 +241,9 @@ function displayProfile(authorId) {
                 followButton.innerHTML = "Follow";
             }
 
-            followButton.addEventListener('click', () => updateFollowStatus(authorId))
+            followButton.addEventListener('click', () => updateFollowStatus(authorId));
 
         }
     })
 
 }
-
-<div class="card mb-3">
-                <div class="row no-gutters">
-                <div class="col-md-8">
-                    <div class="card-body">
-                    <a class="card-author" href="{% url 'profile' post.author.username %}">{{ post.author }}<a>
-                    <p class="card-content" id="post-content-{{ post.id }}">{{ post.content }}</p>
-                    <p class="card-datetime">{{ post.datetime }}</p>
-                    {% if request.user == post.author %}
-                        <button class="btn btn-sm btn-outline-primary" id="edit-post-btn-{{ post.id }}">Edit</button>
-                    {% endif %}
